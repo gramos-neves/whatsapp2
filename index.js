f91ffbd8650917abc8b36d1749e077045fd618a1
xxjamisxx <gui_haunter@hotmail.com>
2022-12-20 14:52:17 -0300

    2

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ const express = require('express');
 const app = express()
 
 
+const agendas = []
+
 
 app.listen(8000, () => {
+    console.log(agendas)
     console.log("listen port 8000")
 })
 
 
 
 app.get("/teste", (req,res) => {
-
+    console.log("teste")
     res.send("ok")
 })
