7e42876aa7c39aef596ec927691fe7f8dd433767
xxjamisxx <gui_haunter@hotmail.com>
2023-01-19 14:26:44 -0300

    a

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ app.post("/webhook", async  (req,res) => {
                         let text_id = body_param.entry[0].changes[0].value.messages?.[0].id;
                         
                         console.log( typeof(msg_body))
-                        console.log((msg_body == '/help')) 
+                        console.log((msg_body !== '/help')) 
 
                         if(msg_body == '/help'){
                             console.log("help")
