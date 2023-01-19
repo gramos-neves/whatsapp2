b478a4f7e47d21077636676a210a56970f194d0c
xxjamisxx <gui_haunter@hotmail.com>
2023-01-19 14:07:57 -0300

    12

--- index copy.js
+++ index copy.js
@@ -58,11 +58,6 @@ app.post("/webhook", async  (req,res) => {
                     case 'text':
                         let msg_body = req.body.entry[0].changes[0].value.messages?.[0].text;
                         let text_id = body_param.entry[0].changes[0].value.messages?.[0].id;
-                          
-                        if(msg_body == "/help"){
-                            console.log("help")
-                        }
-
                         await acaonaopermitidaNew(from,phon_no_id,text_id);
                         break;
                     case 'button':
