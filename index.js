a4690aa5af4b639cd9642f74437b4e6c75640152
xxjamisxx <gui_haunter@hotmail.com>
2023-01-26 14:50:02 -0300

    a

--- index.js
+++ index.js
@@ -184,7 +184,7 @@ app.post("/powerautomate", (req, res) => {
  // arrStatus = [];
  
 
-   console.log(req.body)
+   console.log(req)
  
    res.status(200).send(); 
    //res.status(200).send(statusNew);
